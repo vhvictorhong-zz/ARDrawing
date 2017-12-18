be548d892dc17769acf08917b54212a7854c0470
victorhong1988 <victorhong1988@gmail.com>
2017-12-17 22:29:37 -0500

    Setup ARKi Scene

--- ARDrawing/ViewController.swift
+++ ARDrawing/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.showsStatistics = true
+        self.sceneView.session.run(configuration)
+    
     }
 
     override func didReceiveMemoryWarning() {
